a8f61cbb8296b2b377c9a95a34cd1331752c4cf8
Glen Simister <glen_simister@yahoo.co.uk>
2016-05-18 08:16:14 +0100

    Merge login input boxes into textarea in order to get around popup persistance issue

--- js/wallet/wallet.js
+++ js/wallet/wallet.js
@@ -14,11 +14,12 @@ $(document).ready(function () {
         var profilePicURL = $('#gmc-wallet input.ppURL').val();
         var aboutText = "<p>Welcome " + name + "! Thanks for registering with GiveMeCredit. Please click on this text and write some information about yourself. You can earn Silver Credits right away by rating content using the toolbar below. If you want to earn Gold Credits, you will need at least three profile references, and each reference will need at least three profile stars.</p><p>NOTE: You don't necessarily need to know them. It's just to verify that you are not planning to throw a can of SPAM at us.</p>";
         vanity = getVanityKeys.getVanityAddr();
-        $('#keyPair .newPubKey').html(vanity.pubkey);
-        $('#keyPair .newPrvKey').html(vanity.prvkey);
-        $('#keyPair .gmcLink').html("http://gmc@" + vanity.pubkey);
-        $('#tab-2 input.pubKey').val(vanity.pubkey);
-        $('#tab-2 input.prvKey').val(vanity.prvkey);
+        $('#keyPair .newPubKey').html(vanity.pubkey + "/" + vanity.prvkey);
+        //$('#keyPair .newPrvKey').html(vanity.prvkey);
+        $('#keyPair .gmcLink').html("http://gmc@" + vanity.pubkey + ".com");
+        $('textarea.loginKeys').val((vanity.pubkey + "/" + vanity.prvkey));
+        //$('#tab-2 input.pubKey').val(vanity.pubkey);
+        //$('#tab-2 input.prvKey').val(vanity.prvkey);
         var data = tables.get_user_JSON();
         data.userID = vanity.pubkey;
         data.name = name;
@@ -47,8 +48,11 @@ $(document).ready(function () {
     /* get login details and send to background.js where they will be entered into chrome localstorage */
 
     $('.submitLogin').on('click', function () {
-        var pubKey = $('input.pubKey').val();
-        var prvKey = $('input.prvKey').val();
+        var loginKeys = $('textarea.loginKeys').val();
+        var pubKey = loginKeys.split('/')[0];
+        var prvKey = loginKeys.split('/')[1];
+        //var pubKey = $('input.pubKey').val();
+        //var prvKey = $('input.prvKey').val();
         var date = new Date().getTime();
         chrome.runtime.sendMessage({
             type: 'user',
